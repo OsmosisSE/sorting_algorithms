34e5c09dfb925b770bc8f27f27316fb3bdcc6d1d
OsmosisSE <sirosmosis1@gmail.com>
2024-01-20 22:11:07 -0800

    Selection sort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -29,3 +29,19 @@ void selection_sort(int *array, size_t size)
 		}
 	}
 }
+
+/**
+ * swap - Function that swaps two values
+ *
+ * @a: Fisrt value
+ * @b: Second value
+ * Return: 0
+ */
+void swap(int *a, int *b)
+{
+	int tmp;
+
+	tmp = *b;
+	*b = *a;
+	*a = tmp;
+}
